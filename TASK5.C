68a04d45abe70040fa2ef1db96f103b200695d42
bhuvankachhadiya <137286464+bhuvankachhadiya@users.noreply.github.com>
2023-07-06 16:47:22 +0530

    Add files via upload

---
+++ TASK5.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int n,c,rev=0,s;
+clrscr();
+printf("enter number n=");
+scanf("%d",&n);
+c=n;
+while(c>0)
+{
+s=c%10;
+rev= (rev*10)+s;
+c=c/10;
+}
+if(n==rev)
+{
+printf("%d is palindrom number",n);
+}
+else
+{
+printf("%d is not palindrom number",n);
+}
+
+getch();
+}
+
